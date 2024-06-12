8919b618caa3b4555c3664895d543010f5396d8d
Eduardo Gil <egpheel@users.noreply.github.com>
2024-06-12 22:13:04 +0100

    changed cipher to Cipher

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,4 +1,4 @@
-# Caesar cipher by Eduardo Gil - An assignment for The Odin Project
+# Caesar Cipher by Eduardo Gil - An assignment for The Odin Project
 
 def caesar_cipher(text = '', shift = 0)
   alphabet = %w[a b c d e f g h i j k l m n o p q r s t u v w x y z]
